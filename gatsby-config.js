1683241a513c1234cd73c84177286b28cc1c922d
Simon du Preez <s.dupreez@crimsoneducation.org>
2019-05-24 14:10:12 +1200

    Add tracking id

--- gatsby-config.js
+++ gatsby-config.js
@@ -54,7 +54,7 @@ module.exports = {
     {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
-        //trackingId: `ADD YOUR TRACKING ID HERE`,
+        trackingId: "UA-70937828-1",
       },
     },
     {
